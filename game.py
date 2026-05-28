60238c91fdc38d9d618eb936497d1193ed341f19
dapoulsen <dapoulsen14@gmail.com>
2026-05-28 21:02:44 +0200

    basic game logic start

--- game.py
+++ game.py
@@ -1,10 +1,27 @@
 from hero import *
+from readchar import readkey, key
 
 class Game:
     def __init__(self):
         self.status = False
         self.rounds = 0
         self.hero = None
+    
+    def start_menu_decider(self):
+        print("You have the option to\n" \
+        "[I]nspect your here\n" \
+        "[U]pgrade a piece of equipment\n")
+        k = readkey()
+        if k == "i" or k == "I":
+            print("Your inv")
+        elif k == "u" or k =="U":
+            print("Your upgrade")
+    
+    def inspect_hero(self):
+        pass
+
+    def upgrade_equipment_prompt(self):
+        pass
 
     def print_welcome_message(self):
         self.hero = Hero()
@@ -17,3 +34,8 @@ class Game:
     def start_game(self):
         self.status = True
         self.print_welcome_message()
+
+    def end_game(self):
+        print("Thanks for playing!!")
+        self.status = False
+    
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from game import *
+from readchar import readkey, readchar
 
 def main():
     game = Game()
 
     game.start_game()
+    while (game.status):
+        game.start_menu_decider()
+        k = readkey()
+        if k == "x":
+            game.end_game()
+        
+
 
 if __name__ == "__main__":
     main()
